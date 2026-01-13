60a5b26e4536ca4e7cae2b96e158aa7d609276fe
Brian Clinkenbeard <brianclinkenbeard@users.noreply.github.com>
2026-01-12 23:39:03 -0500

    nvim: sw 2, indent guides

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,38 @@
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+local o = vim.opt
+local api = vim.api
 
-vim.opt.undofile = true
+o.number = true
+
+o.ignorecase = true
+o.smartcase = true
+
+o.undofile = true
+
+o.expandtab = true
+o.shiftwidth = 2
+o.tabstop = 2
+
+-- LIST / LISTCHARS --
+o.list = true
+o.listchars:append { lead = "·" }
+
+-- indent guides based on effective shiftwidth
+local function set_indent()
+  local sw = vim.fn.shiftwidth()
+  local lms = "▏" .. string.rep(" ", sw - 1)
+  o.listchars:append { leadmultispace = lms }
+end
+api.nvim_create_autocmd({ "OptionSet", "FileType" }, {
+  pattern = { "tabstop", "*" },
+  callback = set_indent
+})
+set_indent()
+
+-- don't show trail in insert mode
+api.nvim_create_autocmd("InsertEnter", {
+  callback = function() o.listchars:remove("trail") end
+})
+api.nvim_create_autocmd("InsertLeave", {
+  callback = function() o.listchars:append({ trail = '-' }) end
+})
+
